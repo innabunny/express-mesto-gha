7004855629a9e4de1ce8b71ead872495bb4b1149
Inna Gosteva <innagosteva31@yandex.ru>
2023-01-09 17:46:14 +0500

    1

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/card');
-const bodyParser = require('body-parser');
+
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
@@ -28,5 +29,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  console.log(`App listening on port ${PORT}`);
 });
