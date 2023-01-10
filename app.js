49474e8491e998041d12a0020ed11ee1dfdbaed0
Inna Gosteva <innagosteva31@yandex.ru>
2023-01-10 15:34:06 +0500

    2

--- app.js
+++ app.js
@@ -6,24 +6,30 @@ const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/card');
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
-const { PORT = 3000 } = process.env;
-
-const app = express();
-
 app.use(helmet());
-app.use(rateLimit);
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
   req.user = {
     _id: '63bbe4266ed3b482ae9ade6b',
   };
   next();
 });
-
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 app.use((req, res) => {
