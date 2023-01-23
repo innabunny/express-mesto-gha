c773c6561a8c48e999b9b3d01a1673a28bea5315
Inna Gosteva <innagosteva31@yandex.ru>
2023-01-23 18:52:12 +0500

    1

--- controllers/users.js
+++ controllers/users.js
@@ -124,7 +124,6 @@ module.exports.login = (req, res, next) => {
           const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
           res.status(SUCCESS).cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
-            sameSite: true,
             httpOnly: true,
           }).send({ message: 'Успешная регистрация' });
         })
