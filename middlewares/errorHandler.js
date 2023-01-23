0f58dc32a428f89b95db79fc1b7c2ce56980f535
Inna Gosteva <innagosteva31@yandex.ru>
2023-01-23 22:08:55 +0500

    1

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,14 @@
-module.exports = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   if (err.code === 11000) {
     res.status(409).send({ message: 'Такой пользователь уже существует' });
+    return;
   }
   if (!err.statusCode) {
     next(err);
   } else {
     res.status(err.statusCode).send({ message: err.message });
+    return;
   }
   res.status(500).send({ message: 'Ошибка сервера' });
 };
+module.exports = errorHandler;
